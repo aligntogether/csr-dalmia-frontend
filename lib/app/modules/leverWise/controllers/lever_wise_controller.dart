9ba84d04e6e6584ab577a8b43d598e74c3bc138f
manav <manavkhadka2004@gmail.com>
2024-01-12 16:31:21 +0530

    levervise report api integrated and export to excel

--- lib/app/modules/leverWise/controllers/lever_wise_controller.dart
+++ lib/app/modules/leverWise/controllers/lever_wise_controller.dart
@@ -7,20 +7,35 @@ class LeverWiseController extends GetxController {
     openMenu.value = !openMenu.value;
     update();
   }
-  List<String> locations = [
-    'Diksha',
-    'ITI',
-    'Water',
+  List<Map<String, Map<String, dynamic>>>? leverWiseApiReportList;
+
+  void updateLeverWiseApiReportList(List<Map<String, Map<String, dynamic>>> leverWiseApiReportList) {
+    this.leverWiseApiReportList = leverWiseApiReportList;
+    update(["add"]);
+  }
+
+  List<String> levers = [
     'Agriculture',
     'Horticulture',
-    'ME',
-    'LS ',
-    'SGH  ',
-    'FPO',
-    'Other',
-    'Total',
-
+    'Livestock',
+    'Water',
+    'IGA',
+    'Non DIKSHA Skills',
+    'Micro Enterprise',
   ];
+
+  var allLocations = ["Dalmiapuram", "Ariyalur", "Belagaum", "Kadapa", "Chandrapur", "SOUTH",
+    "Megalaya", "Umrangso", "Jagiroad", "Lanka","NE",
+    "Cuttak", "Medinipur", "Bokaro", "Rajgangpur", "Kholapur", "EAST", "CEMENT",
+    "Nigohi", "Ramgarh", "Jawaharpur", "Ninaidevi", "Kholapur", "SUGAR", "PANIND"];
+
+  // var allLocations = ["Dalmiapuram", "Ariyalur", "Belagaum", "Kadapa", "Chandrapur",
+  //   "Megalaya", "Umrangso", "Jagiroad", "Lanka",
+  //   "Cuttak", "Medinipur", "Bokaro", "Rajgangpur", "Kholapur",
+  //   "Nigohi", "Ramgarh", "Jawaharpur", "Ninaidevi", "Kholapur",];
+
+
+
   List<int> DPM = [
     128036,
     37765,
